c995800230c233435186d12182194bb9c3ddf5fd
leanfj <leanfj@hotmail.com>
2018-08-22 23:01:26 -0300

    Identação de código

--- routes/produtosRoute.js
+++ routes/produtosRoute.js
@@ -6,9 +6,13 @@ const autorizacao = require("../config/firebase").usuarioLogado;
 const controller = require("../controllers/produtosController");
 
 router.get("/", autorizacao, controller.listaProdutos);
+
 router.get("/:slug", autorizacao, controller.produtoBySlug);
+
 router.post("/", autorizacao, controller.cadastraProduto);
+
 router.put("/:id", autorizacao, controller.atualizaProduto);
+
 router.delete("/:id", autorizacao, controller.removeProduto);
 
 module.exports = router;
